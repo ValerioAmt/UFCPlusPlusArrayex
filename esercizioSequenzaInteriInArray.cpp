7482835825554c7d7c42ed8dccdcb9a8a321e909
valerio <valerioamato@hotmail.it>
2023-02-28 15:04:42 +0100

    prova esercizio sequenze

--- esercizioSequenzaInteriInArray.cpp
+++ esercizioSequenzaInteriInArray.cpp
@@ -5,8 +5,19 @@ using namespace std;
 
 int main(){
 
-
-
+int array[] = {1,2,3,5,4,7}
+int quanteSequenze = 0;
+int i = 0;
+int lung = sizeof(array[i])/sizeof(array[0]);
+int lunghezza = lung;
+for (i = 0; i < lunghezza; i ++){
+    if(array[i]>= array[i+1]){
+    
+    }else{
+        quanteSequenze + 1;
+    }
 
     
+}
+cout << quanteSequenze;
 }
