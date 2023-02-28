3f6ffa7c65b93abaeb65c714f6eac73c813008e9
valerio <valerioamato@hotmail.it>
2023-02-28 15:42:51 +0100

    es2 funzione

--- esercizioSequenzaInteriInArray.cpp
+++ esercizioSequenzaInteriInArray.cpp
@@ -5,19 +5,40 @@ using namespace std;
 
 int main(){
 
-int array[] = {1,2,3,5,4,7}
-int quanteSequenze = 0;
+int array[] = {1,2,3,5,4,7};
+int quante ;
 int i = 0;
 int lung = sizeof(array[i])/sizeof(array[0]);
 int lunghezza = lung;
+int y = 0;
+
+while (i < lunghezza){
+      for(y=0;y < lunghezza; y++){
+         if(array[y] < array[y+1]){
+            quante++;
+         }else{
+           
+         }
+
+            
+
+      }
+
+
+}
+cout << quante;
+
+}
+/*
 for (i = 0; i < lunghezza; i ++){
     if(array[i]>= array[i+1]){
     
     }else{
-        quanteSequenze + 1;
+        quante++ ;
     }
 
     
 }
-cout << quanteSequenze;
+cout << quante <<endl;
 }
+*/
--- funzioneRestituisceValoreMaxinArray.cpp
+++ funzioneRestituisceValoreMaxinArray.cpp
@@ -3,18 +3,26 @@
 
 using namespace std;
 
+int calcolaMax(int array[],int lunghezza){
+
+  for(int i = 1; i < lunghezza; i ++){
+    if (array[i] > valMax){
+       valMax = array[i];
+} 
+  }
+  return valMax;
+}
 int main(){
 
  int array[] = {4,2,78};
  int lunghezza = sizeof(array)/sizeof(array[0]);
  int lung = lunghezza;
 
- int valMax = array[0];
-
+ int valMax = calcolaMax(array, lung);
+/*
 for(int i = 1; i < lunghezza; i ++){
     if (array[i] > valMax){
        valMax = array[i];
+*/cout << calcolaMax(array, lung) <<endl;
 }
-}
-  cout << valMax<<endl;
-}
+  
