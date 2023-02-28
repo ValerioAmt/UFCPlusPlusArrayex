3b2e562ed1c5a98aa8ac4bfeb55080361f1d2e99
valerio <valerioamato@hotmail.it>
2023-02-28 14:26:27 +0100

    completo es 4

--- funzioneRestituiscePari.cpp
+++ funzioneRestituiscePari.cpp
@@ -6,11 +6,12 @@ int main(){
 int array[] = {58,64,39,77,3,7};
 int lung = sizeof(array)/sizeof(array[0]);
 int lunghezza = lung;
-int arrayPari[]
+int arrayPari[v];
+int v = 0;
       for(int i =0;i < lung;i++){
           if (array[i]%2==0){
-            cin >> ArrayPari;
-          }
+                cin << arrayPari[v];
+      v++          }
 
 }
 }
