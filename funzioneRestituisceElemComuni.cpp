d2c6413de1d386fccb3e2fbd0d6fb19e841c70a0
valerio <valerioamato@hotmail.it>
2023-02-28 16:25:06 +0100

    prova funzion esercizio 7

--- funzioneRestituisceElemComuni.cpp
+++ funzioneRestituisceElemComuni.cpp
@@ -3,27 +3,33 @@
 
 using namespace std;
 
-
-int main (){
-int arrayPrimo[] = {1,2,3,4};
-int arraySecondo[] = {2,4,6,8};
+int controllaDoppie(int array, int arrayDue, int lunghezzaUno, int lunghezzaDue)
 int contatore = 0;
-int lunghezzaprimo = sizeof(arrayPrimo)/(sizeof(arrayPrimo[0]));
-int lunghezzasecondo =sizeof(arraySecondo)/sizeof(arraySecondo[0]); 
-   for(int i=0;i<lunghezzaprimo;i++){
-    for(int y=0;y<lunghezzasecondo;y++){
-           if(arrayPrimo[i]==arraySecondo[y]){
-            contatore ++;
-           }
-             
 
+    for (int i = 0; i < lunghezzaUno; i++)
+    { // confronto i due array utilizzando due indici (i,y)
+        for (int y = 0; y < lunghezzaDue; y++)
+        {
+            if (array[i] == arrayDue[y])
+            {
+                contatore++;
+            }
+        }
+    }
 
-    }         
+    int main()
+    {
+        int arrayPrimo[] = {1, 2, 3, 4};
+        int arraySecondo[] = {2, 4, 6, 8};
+        int contatore = 0;
+        int lunghezzaprimo = sizeof(arrayPrimo) / (sizeof(arrayPrimo[0]));
+        int lunghezzasecondo = sizeof(arraySecondo) / sizeof(arraySecondo[0]);
+        /* for(int i=0;i<lunghezzaprimo;i++){                                //confronto i due array utilizzando due indici (i,y)
+          for(int y=0;y<lunghezzasecondo;y++){
+                 if(arrayPrimo[i]==arraySecondo[y]){
+                  contatore ++;
+                 }
+          */
 
-   }
-
-    cout << contatore<<endl;
-
-
-
-}
+        cout << controllaDoppie(arrayPrimo,arraySecondo,lunghezzaPrimo,lunghezzaSecondo) << endl;
+    }
