2227234205d3f741b1cc3ccc5f922a32451e15f0
valerio <valerioamato@hotmail.it>
2023-03-01 14:46:07 +0100

    test ultimo

--- dueArrayConsecutivi.cpp
+++ dueArrayConsecutivi.cpp
@@ -11,11 +11,11 @@ int x = sizeof(arrayPrimo)/sizeof(arrayPrimo[0]);
 int y = sizeof(arraySecondo)/sizeof(arraySecondo[0]);
 int arrayFinale[x+y] ;                                 //creo spazio esatto per ultimo array
                for (int i = 0; i < x;i ++){
-                      arrayFinale[i] += arrayPrimo[i];
+                      arrayFinale[i] = arrayPrimo[i];
                }
 
                for (int i = 0; i < x; i++){
-                arrayFinale[i] += arraySecondo[i];
+                arrayFinale[i] = arraySecondo[i];
                }
 
             cout << arrayFinale;
