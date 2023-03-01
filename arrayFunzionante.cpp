b6c5c9dc5b5b517047902bbb71bff666e71c18d0
valerio <valerioamato@hotmail.it>
2023-03-01 14:44:54 +0100

    prova ultimo esercizio

--- arrayFunzionante.cpp
+++ arrayFunzionante.cpp
@@ -3,29 +3,27 @@
 
 using namespace std;
 
-
-
-//in questo tipo di esercizi, IMPORTANTE STABILIRE DIMENSIONI DI ARRAY PRIMA DI UTLIZZARLO!!!
+// in questo tipo di esercizi, IMPORTANTE STABILIRE DIMENSIONI DI ARRAY PRIMA DI UTLIZZARLO!!!
+// IL PRIMO FOR A RIGA 16, STABILISCE LA DIMENSIONE DEL SECONDO ARRAY, DOVE CI SARANNO SOLO NUMERI PARI!
 int main()
 {
 
-    int array[] = {3, 7, 65, 26}; // creo array da analizzare
+    int array[4] = {3, 7, 54, 26}; // creo array da analizzare
     int quantiPari = 0;
-    int lunghezza = sizeof(array) / sizeof(array[0]);
+    int lunghezza = sizeof(array);
+
     for (int i = 0; i < lunghezza; i++)
     {
         if (array[i] % 2 == 0)
         {
             quantiPari++;
-        }else {
-            break;
         }
     }
-    
-    int arrayPari[quantiPari] ;   //creo il secondo array che avrà tanti caratteri quati sono i numri pari sull'array dato
-        for(int i = 0;i < quantiPari;i++){
-              if(array[i] % 2 == 0)
-               cin >> arrayPari[i];
-             }
-            cout << arrayPari <<endl;
-        }
+
+    int arrayPari[quantiPari]; // creo il secondo array che avrà tanti caratteri quati sono i numri pari sull'array dato
+    for (int i = 0; i < quantiPari; i++)
+    {
+        cin >> arrayPari[i];
+    }
+    cout << arrayPari[quantiPari] << endl;
+}
--- dueArrayConsecutivi.cpp
+++ dueArrayConsecutivi.cpp
@@ -9,13 +9,13 @@ int arrayPrimo[] = {1,2,3};
 int arraySecondo[] = {4,5,6};
 int x = sizeof(arrayPrimo)/sizeof(arrayPrimo[0]);
 int y = sizeof(arraySecondo)/sizeof(arraySecondo[0]);
-int arrayFinale = y + x;  //creo spazio esatto per ultimo array
+int arrayFinale[x+y] ;                                 //creo spazio esatto per ultimo array
                for (int i = 0; i < x;i ++){
-                      arrayFinale += arrayPrimo[i];
+                      arrayFinale[i] += arrayPrimo[i];
                }
 
                for (int i = 0; i < x; i++){
-                arrayFinale += arraySecondo[i];
+                arrayFinale[i] += arraySecondo[i];
                }
 
             cout << arrayFinale;
